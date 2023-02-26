74d9abed928a4bb3094f083147af238001d019e8
oranssy <oranssy123@gmail.com>
2023-02-26 23:00:31 +0900

    230226
    웹디자인 기능사 실기 예제 _A1

--- Exam/assets/js/scriptA1.js
+++ Exam/assets/js/scriptA1.js
@@ -1,11 +1,11 @@
 // header menu
 $(function(){
     $('.menu_main').mouseenter(function(){
-        $('.menu_sub').slideDown(900);
+        $('.menu_sub').slideDown(800);
     });
 
     $('.menu_main').mouseleave(function(){
-        $('.menu_sub').stop().slideUp(600);
+        $('.menu_sub').stop().slideUp(500);
     });
 });
 
@@ -16,3 +16,28 @@ setInterval(function(){
         $('.slide_wrap').css({'margin-top':'0'});
     });
 }, 3000);
+
+// tab_menu
+$(function(){
+    $('.tab_menuTit li').click(function(){
+
+        var idx = $(this).index();
+
+        $('.tab_menuDesc ul').hide().removeClass('on');
+        $('.tab_menuDesc ul').eq(idx).show().addClass('on');
+
+        $('.tab_menuTit li').removeClass('on');
+        $(this).addClass('on');
+    });
+});
+
+// popUp
+$(function(){
+    $('.notice li:nth-child(1)').click(function(){
+        $('.popUp').show();
+    });
+
+    $('.popUp_cont button').click(function(){
+        $('.popUp').hide();
+    });
+});
